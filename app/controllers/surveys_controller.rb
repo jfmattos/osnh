ff7bf7f00cd4e75d749790429aba97c17c6e0bef
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-09-03 16:57:36 -0300

    survey new/create/destroy methods created

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,6 @@
 class SurveysController < ApplicationController
+  before_action :set_survery, only: %i[destroy]
+  
   def index
     @survey = Survey.all
   end
@@ -11,9 +13,17 @@ class SurveysController < ApplicationController
     @survey = Survey.new(survey_params)
   end
 
+  def destroy
+    @survey.destroy
+  end
+
   private
 
   def survey_params
     params.require(:survey).permit(:title, :interval_days)
+  end
+
+  def set_survery
+    @survey = Survey.find(params[:id])
   end
 end
