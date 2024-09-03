b1fc1f818a55593e8ad24bb815c8c02c82805027
jasmim mattos <jasmimfm@gmail.com>
2024-09-03 17:24:31 -0300

    Add dotenv-rails

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ApplicationRecord
   validates :name, :address, :services, :opening_hours, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
