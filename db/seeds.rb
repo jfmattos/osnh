3de3cbb924354d2d656147afdbbbaa07a255641d
jasmim mattos <jasmimfm@gmail.com>
2024-09-03 12:33:27 -0300

    routes to places,index page and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "creating places"
+
+Place.destroy_all
+
+Place.create!(
+  name: "smart fit",
+  address: "Av. Nossa Senhora de Copacabana",
+  opening_hours: "06:00 - 22:00",
+  email: "smartfit@smart.com",
+  phone_number: "1234-5678",
+  services: "gym classes"
+
+)
+
+puts "places created!"
