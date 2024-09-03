85ccdff17b1594a1f6c34e9967979b1044b9c052
jasmim mattos <jasmimfm@gmail.com>
2024-09-03 18:40:34 -0300

    implementing geocoding

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,14 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.geocoded
+    @markers = @places.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude,
+        info_window: render_to_string(partial: "popup", locals: {place: place}),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
+
   end
 end
