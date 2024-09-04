497fef3f4388638dbd82cc8288935f9fedb69edc
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-09-04 15:45:56 -0300

    form initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
-  resources :surveys, only: [:index]
+  resources :surveys, only: [:index, :new]
 end
