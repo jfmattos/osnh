cb5916b842f0a7a17c6a4d01b0b78834433bc2b3
jasmim mattos <jasmimfm@gmail.com>
2024-09-04 17:40:10 -0300

    improving responsive cards

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+
+    if params[:query].present?
+      @places = @places.search_by(params[:query])
+    end
+
     @markers = @places.map do |place|
       {
         lat: place.latitude,
@@ -9,10 +14,5 @@ class PlacesController < ApplicationController
         marker_html: render_to_string(partial: "marker")
       }
     end
-
-    if params[:query].present?
-      @places = @places.search_by(params[:query])
-    end
-
   end
 end
