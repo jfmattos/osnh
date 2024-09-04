ad33d10fcb038daa89cd860dcc2195429142d6b7
Thomas Wasson <gtwasson@gmail.com>
2024-09-04 16:39:10 -0400

    resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,14 @@
 class ResourcesController < ApplicationController
+
   def index
     @resources = Resource.all
+  end
+
+  # def new
+  #   @resource = Resource.new
+
+
+  def show
+    @resource = Resource.find(params[:id])
   end
 end
