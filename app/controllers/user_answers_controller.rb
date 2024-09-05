df2c8c634b965a6a8ea6236f5cf5840bf6b5c1f2
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-09-05 17:54:25 -0300

    form user story partial + seeds file updated

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -1,12 +1,30 @@
 class UserAnswersController < ApplicationController
+  before_action :set_survery
+  
   def create
     @user_answer = UserAnswer.new(user_answer_params)
     @user_answer.user = current_user
-    raise
+    # question_index >= @survey.questions.length - 1
+    if @user_answer.save
+      question_index = params["user_answer"]["question_index"]
+      redirect_to new_survey_user_answer_path(@survey, question_index: question_index)
+    end
+
+    # raise
   end
-
+  
+  def new
+    # raise
+    @question = @survey.questions[params[:question_index].to_i]
+    @user_answer = UserAnswer.new()
+  end
+  
   private
-
+  
+  def set_survery
+    @survey = Survey.find(params[:survey_id])
+  end
+  
   def user_answer_params
     params.require(:user_answer).permit(:answer_id)
   end
--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -2,5 +2,5 @@ class UserAnswer < ApplicationRecord
   belongs_to :answer
   belongs_to :user
 
-  validates :reply_date, presence: true
+  # validates :reply_date, presence: true
 end
