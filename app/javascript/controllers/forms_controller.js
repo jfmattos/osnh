f278a07b0f1782fb7ed121d3b8d546da4dc6a240
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-09-10 10:14:30 -0300

    js controller functional

--- app/javascript/controllers/forms_controller.js
+++ app/javascript/controllers/forms_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="forms"
 export default class extends Controller {
+  static targets = ["form"]
+  
   connect() {
+  }
+  submit(event) {
+    this.formTarget.submit()
   }
 }
