8684e868add7da25fa3d00d3b8680df547f67a72
Guilherme Rizzo <grsp+lewagon73dd8znudr@pm.me>
2024-09-10 15:58:10 -0300

    new method added to Users controller + migration file created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def find_question
+    whoqol_01 = Survey.last.question.first
+    UserAnswer.joins(answer: :question)
+              .where(answer: { question_id: whoqol_01 }, reply_date: Date.today)
+  end
+
   def show
     @my_diagnoses = current_user.diagnoses
   end
@@ -8,7 +14,14 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :cns, :address, :phone_number, :email, :admin, :photo)
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :cns,
+                                 :address,
+                                 :phone_number,
+                                 :email,
+                                 :admin,
+                                 :photo)
   end
 
 end
---
+++ db/migrate/20240910185705_replay_date_string_to_date.rb
@@ -0,0 +1,4 @@
+class ReplayDateStringToDate < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
