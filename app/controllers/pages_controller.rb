b9951da9a315e6a3e168c7b43ea1339700fa4126
Thomas Wasson <gtwasson@gmail.com>
2024-09-10 16:50:47 -0400

    landingpage-navbar-links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @user = current_user.id
-    @surveys = Survey.all
-    @places = Place.all
+
+  end
+
+  def landing
+
   end
 end
