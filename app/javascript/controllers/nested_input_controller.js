338d0b74a7761719a189498a9b001402dc892c35
Diana Portela <dianasaddi@gmail.com>
2024-09-10 17:25:20 -0300

    diagnosis without med fixed bug

--- app/javascript/controllers/nested_input_controller.js
+++ app/javascript/controllers/nested_input_controller.js
@@ -7,7 +7,23 @@ export default class extends Controller {
     medicationId: Number
   }
   connect() {
-
+    if (this.medListTarget.childElementCount === 0) {
+      const firstMed = `<div class="row" data-nested-input-target="medFields">
+    <div class="col-11">
+      <div class="row">
+        <div class="col-5 border-0 rounded-5">
+          <div class="mb-3 string required diagnosis_medications_name"><input class="form-control string required" placeholder="Medication" type="text" name="diagnosis[medications_attributes][0][name]" id="diagnosis_medications_attributes_0_name"></div>
+        </div>
+        <div class="col-6 border-0 rounded-5">
+          <div class="mb-3 string required diagnosis_medications_daily_dosage"><input class="form-control string required" placeholder="Daily dosage" type="text" name="diagnosis[medications_attributes][0][daily_dosage]" id="diagnosis_medications_attributes_0_daily_dosage"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-1">
+    </div>
+  </div>`;
+      this.medListTarget.insertAdjacentHTML("beforeend", firstMed);
+    }
   };
 
   add(event) {
